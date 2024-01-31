1002f84ce6f078e293b5b7882e9a5c3e53a5bdff
menisadi <menisadi@gmail.com>
2024-01-31 14:40:45 +0200

    continous movement

--- main.lua
+++ main.lua
@@ -22,134 +22,126 @@ local landTile = love.graphics.newImage("landTile.png")
 local friendImage = love.graphics.newImage("Friend.png")
 
 local dialogue = {
-    text = "",
-    timer = 0,
-    duration = 3
+	text = "",
+	timer = 0,
+	duration = 3,
 }
 
+local keysPressed = {} -- Table to store keys currently being pressed
+
+function love.keypressed(key)
+	keysPressed[key] = true -- Mark the key as pressed
+end
+
+function love.keyreleased(key)
+	keysPressed[key] = nil -- Remove the key from the table when released
+end
+
 function love.load()
-    resetFriendPosition()
-    -- Initialize dialogue variables
-    dialogue.text = ""
-    dialogue.displayedText = ""
-    dialogue.currentIndex = 0
-    dialogue.timer = 0
-    dialogue.duration = 0.05
+	resetFriendPosition()
+	-- Initialize dialogue variables
+	dialogue.text = ""
+	dialogue.displayedText = ""
+	dialogue.currentIndex = 0
+	dialogue.timer = 0
+	dialogue.duration = 0.05
 end
 
 function resetFriendPosition()
-    friend.x = math.random(0, love.graphics.getWidth() - friend.width)
-    friend.y = math.random(0, love.graphics.getHeight() - friend.height)
-end
-
-function love.keypressed(key)
-    local step_size = 20
-    if key == "up" then
-        player.destY = player.y - step_size
-        -- player.orientation = math.pi / 2
-    elseif key == "down" then
-        player.destY = player.y + step_size
-        -- player.orientation = math.pi * 3 / 2
-    elseif key == "left" then
-        player.destX = player.x - step_size
-        -- player.orientation = 0
-    elseif key == "right" then
-        player.destX = player.x + step_size
-        -- player.orientation = math.pi
-    end
+	friend.x = math.random(0, love.graphics.getWidth() - friend.width)
+	friend.y = math.random(0, love.graphics.getHeight() - friend.height)
 end
 
 local function lerp(a, b, t)
-    if b == nil then return a end
-    return a * (1 - t) + b * t
+	if b == nil then
+		return a
+	end
+	return a * (1 - t) + b * t
 end
 
 function displayFriendDialogue(text)
-    dialogue.text = text
-    dialogue.displayedText = ""
-    dialogue.currentIndex = 0
-    dialogue.timer = dialogue.duration
+	dialogue.text = text
+	dialogue.displayedText = ""
+	dialogue.currentIndex = 0
+	dialogue.timer = dialogue.duration
 end
 
 function love.update(dt)
-    player.x = lerp(player.x, player.destX, dt * 3)
-    player.y = lerp(player.y, player.destY, dt * 3)
+	-- Update player movement based on keys being pressed
+	local step_size = 20
+	if keysPressed["up"] then
+		player.destY = player.y - step_size
+	elseif keysPressed["down"] then
+		player.destY = player.y + step_size
+	end
+	if keysPressed["left"] then
+		player.destX = player.x - step_size
+	elseif keysPressed["right"] then
+		player.destX = player.x + step_size
+	end
 
-    if player.x < 0 then
-        player.x = love.graphics.getWidth()
-        resetFriendPosition()
-    elseif player.x > love.graphics.getWidth() then
-        player.x = 0
-        resetFriendPosition()
-    end
+	player.x = lerp(player.x, player.destX, dt * 3)
+	player.y = lerp(player.y, player.destY, dt * 3)
 
-    if player.y < 0 then
-        player.y = love.graphics.getHeight()
-        resetFriendPosition()
-    elseif player.y > love.graphics.getHeight() then
-        player.y = 0
-        resetFriendPosition()
-    end
+	if player.x < 0 then
+		player.x = love.graphics.getWidth()
+		resetFriendPosition()
+	elseif player.x > love.graphics.getWidth() then
+		player.x = 0
+		resetFriendPosition()
+	end
 
-    player.frame = player.frame + 6 * dt
-    player.frame = player.frame % 4
+	if player.y < 0 then
+		player.y = love.graphics.getHeight()
+		resetFriendPosition()
+	elseif player.y > love.graphics.getHeight() then
+		player.y = 0
+		resetFriendPosition()
+	end
 
-    -- Check player-friend proximity
-    local distance = math.sqrt((player.x - friend.x)^2 + (player.y - friend.y)^2)
-    if distance < 50 then
-        if dialogue.timer <= 0 then
-            -- displayFriendDialogue("Hello again. Did you know that the time is: " .. os.date("%H:%M:%S"))
-            -- dialogue.text = "Hello again. Did you know that the time is: " .. os.date("%H:%M:%S")
-            dialogue.text = random_bible.random_bible_phrase()
-        end
-        dialogue.timer = dialogue.duration
-    else
-        dialogue.timer = 0
-    end
+	player.frame = player.frame + 6 * dt
+	player.frame = player.frame % 4
 
-    -- Update dialogue text display
-    -- if dialogue.timer > 0 then
-    --     dialogue.timer = dialogue.timer - dt
-    --     if dialogue.timer <= 0 then
-    --         dialogue.currentIndex = dialogue.currentIndex + 1
-    --         dialogue.displayedText = dialogue.text:sub(1, dialogue.currentIndex)
-    --         if dialogue.currentIndex < #dialogue.text then
-    --             dialogue.timer = dialogue.duration
-    --         end
-    --     end
-    -- end    
+	-- Check player-friend proximity
+	local distance = math.sqrt((player.x - friend.x) ^ 2 + (player.y - friend.y) ^ 2)
+	if distance < 50 then
+		if dialogue.timer <= 0 then
+			-- displayFriendDialogue("Hello again. Did you know that the time is: " .. os.date("%H:%M:%S"))
+			-- dialogue.text = "Hello again. Did you know that the time is: " .. os.date("%H:%M:%S")
+			dialogue.text = random_bible.random_bible_phrase()
+		end
+		dialogue.timer = dialogue.duration
+	else
+		dialogue.timer = 0
+	end
+
+	-- Update dialogue text display
+	-- if dialogue.timer > 0 then
+	--     dialogue.timer = dialogue.timer - dt
+	--     if dialogue.timer <= 0 then
+	--         dialogue.currentIndex = dialogue.currentIndex + 1
+	--         dialogue.displayedText = dialogue.text:sub(1, dialogue.currentIndex)
+	--         if dialogue.currentIndex < #dialogue.text then
+	--             dialogue.timer = dialogue.duration
+	--         end
+	--     end
+	-- end
 end
 
 function love.draw()
-    love.graphics.draw(landTile, 0, 0)
-    
-    local frameWidth = player.width / 4
-    local frameHeight = player.height / 4
-    local frameX = (player.frame - 1) * frameWidth
-    love.graphics.draw(
-        spriteSheet,
-        player.x,
-        player.y,
-        0,
-        0.2,
-        0.2,
-        frameX,
-        0
-    )
+	love.graphics.draw(landTile, 0, 0)
 
-    love.graphics.draw(
-        friendImage, 
-        friend.x, 
-        friend.y,
-        0,
-        0.2,
-        0.2
-    )
-    if dialogue.timer > 0 then
-        love.graphics.setColor(0, 0, 0, 0.7) -- Black with 70% opacity
-        love.graphics.rectangle("fill", 0, love.graphics.getHeight() - 50, love.graphics.getWidth(), 50)
-        love.graphics.setColor(1, 1, 1) -- White text
-        love.graphics.printf(dialogue.text, 0, love.graphics.getHeight() - 40, love.graphics.getWidth(), "left")
-        -- love.graphics.printf(dialogue.displayedText, 30, love.graphics.getHeight() - 40, love.graphics.getWidth() - 60, "left")
-    end
+	local frameWidth = player.width / 4
+	local frameHeight = player.height / 4
+	local frameX = (player.frame - 1) * frameWidth
+	love.graphics.draw(spriteSheet, player.x, player.y, 0, 0.2, 0.2, frameX, 0)
+
+	love.graphics.draw(friendImage, friend.x, friend.y, 0, 0.2, 0.2)
+	if dialogue.timer > 0 then
+		love.graphics.setColor(0, 0, 0, 0.7) -- Black with 70% opacity
+		love.graphics.rectangle("fill", 0, love.graphics.getHeight() - 50, love.graphics.getWidth(), 50)
+		love.graphics.setColor(1, 1, 1) -- White text
+		love.graphics.printf(dialogue.text, 0, love.graphics.getHeight() - 40, love.graphics.getWidth(), "left")
+		-- love.graphics.printf(dialogue.displayedText, 30, love.graphics.getHeight() - 40, love.graphics.getWidth() - 60, "left")
+	end
 end
